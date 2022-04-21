4286b472353fdf0291210968405282694f34c470
Jun Guo <gjuoun@gmail.com>
2022-04-21 00:56:00 -0300

    remove ejs, tests pass

--- lib/engines/engines_test.ts
+++ lib/engines/engines_test.ts
@@ -1,15 +1,14 @@
 import { assertEquals, blue } from "../../deps.ts";
 
 import { denjuckEngine } from "./denjuck/denjuck.engine.ts";
-import { ejsEngine } from "./ejs/ejs.engine.ts";
 import { handlebarsEngine } from "./handlebars/handlebars.engine.ts";
 
 Deno.test({
   name: blue("Testing renderDenjuck()"),
-  fn(): void {
+  async fn() {
     const template = `<h1>{{data.name}}</h1>`;
 
-    const actual = denjuckEngine(template, { data: { name: "John" } });
+    const actual = await denjuckEngine(template, { data: { name: "John" } });
     const expect = `<h1>John</h1>`;
     assertEquals(actual, expect);
   },
@@ -17,22 +16,11 @@ Deno.test({
 
 
 Deno.test({
-  name: blue("Testing renderEjs()"),
-  fn(): void {
-    const template = `Hobbies of <%=data.name%>`;
-
-    const actual = ejsEngine(template, { data: { name: "John" } });
-    const expect = `Hobbies of John`;
-    assertEquals(actual, expect);
-  },
-});
-
-Deno.test({
   name: blue("Testing renderHandlebars()"),
-  fn(): void {
+  async fn() {
     const template = `<h1>{{data.name}}</h1>`;
 
-    const actual = handlebarsEngine(template, { data: { name: "John" } });
+    const actual =await handlebarsEngine(template, { data: { name: "John" } });
     const expect = `<h1>John</h1>`;
     assertEquals(actual, expect);
   },
