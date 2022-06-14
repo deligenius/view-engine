f17b0bfda06b9de2e970b99bc10f3a75696942b1
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-06-14 22:14:15 +0200

    fix(oak): use async Deno.readTextFile
    Fixes #32

--- lib/adapters/oak/oak.utils.ts
+++ lib/adapters/oak/oak.utils.ts
@@ -4,7 +4,7 @@ const urlRegex =
 /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
 
-export async function getTemplate(viewRoot: string, fileName: string) {
+export function getTemplate(viewRoot: string, fileName: string): Promise<string> {
   const filePath = path.join(viewRoot, fileName);
-  return Deno.readTextFileSync(filePath);
+  return Deno.readTextFile(filePath);
 }
