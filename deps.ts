a7a4f2b052a1ac91cb40bd8f84d1833e01ca7e82
Luiz Esmiralha <esmiralha@gmail.com>
2022-12-09 09:11:12 -0300

    add pug engine

--- deps.ts
+++ deps.ts
@@ -3,10 +3,13 @@ export * as path from "https://deno.land/std@0.131.0/path/mod.ts";
 
 // test dependencies
 export { green, blue } from "https://deno.land/std@0.131.0/fmt/colors.ts";
-export { assertEquals, assert } from "https://deno.land/std@0.131.0/testing/asserts.ts";
+export {
+  assertEquals,
+  assert,
+} from "https://deno.land/std@0.131.0/testing/asserts.ts";
 
 export { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
-
-export * as eta from 'https://deno.land/x/eta@v1.12.3/mod.ts'
+export * as eta from "https://deno.land/x/eta@v1.12.3/mod.ts";
 export * as dejs from "https://deno.land/x/dejs@0.10.2/mod.ts";
+export * as pug from "https://deno.land/x/pug@v0.1.3/mod.ts";
